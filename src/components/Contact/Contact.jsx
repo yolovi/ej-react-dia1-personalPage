43c3f623b4be2e6943fd3b1a159d5148db994eac
yolovi <yolandalopevi@gmail.com>
2023-07-27 22:57:20 +0200

    modified Contact component, useEffect validation name

--- src/components/Contact/Contact.jsx
+++ src/components/Contact/Contact.jsx
@@ -1,11 +1,31 @@
-import { useState } from "react";
+
+
+  //FIXME: //FUNCIONES. Problema, se salta la validacion si borras y te deja poner menos de 3 caracteres
+  // const handleInputChange = (event) => {
+  //   // console.log(event.target.name); //nombre del input en el que escribo
+  //   // console.log(event.target.value); //lo que escribo
+  //   if (data.name.length +1 < 3) {
+  //     setMessage("Name must be at least 3 characters");
+  //     setBtnDisabled(true);
+  //   } else {
+  //     setMessage(null);
+  //     setBtnDisabled(false);
+  //   }
+  //   setData({
+  //     ...data, //para que no borre los valores que ya había
+  //     [event.target.name]: event.target.value,
+  //   });
+  // };
+
+
+import { useEffect, useState } from "react";
 import { useNavigate } from "react-router-dom";
 
 const Contact = () => {
-    const initialState = {
-        name: "",
-        email: "",
-      };
+  const initialState = {
+    name: "",
+    email: "",
+  };
 
   //ESTADOS DEL COMPONENTE
   //estado del formulario
@@ -16,68 +36,78 @@ const Contact = () => {
   const [message, setMessage] = useState("");
   const navigate = useNavigate(); //inicializamos navigate
 
-// FUNCIONES
-    const handleInputChange = (event) => {
-       // console.log(event.target.name); //nombre del input en el que escribo
-       // console.log(event.target.value); //lo que escribo
-        if (data.name.length < 3) {
-            setMessage("Name must be at least 3 characters");
-            setBtnDisabled(true);
-          } else {
-            setMessage(null);
-            setBtnDisabled(false);
-          }
-        setData({
-          ...data, //para que no borre los valores que ya había
-          [event.target.name]: event.target.value,
-        });
-      };
+   //FIXME: Con useEffect sale desde el inicio el setMessage de validacion y ademas, cuando se envia el formulario no sale el setMessage de enviado con exito sigue apareciendo el del aviso de validacion min 3  caracteres. Se soluciona si no utilizamos el clearForm
 
-      //funcion que limpia el formulario
+  // useEffect para validar el formulario cuando cambie el estado 'data'
+  useEffect(() => {
+    if (data.name.length < 3) {
+      setMessage("Name must be at least 3 characters");
+      setBtnDisabled(true); // Deshabilitar el botón si hay un error de validación
+    } else {
+      setMessage(null);
+      setBtnDisabled(false); // Habilitar el botón si no hay errores de validación
+    }
+  }, [data]);
+
+  // Función que maneja los cambios en los inputs
+  const handleInputChange = (event) => {
+    const { name, value } = event.target;
+
+    setData({
+      ...data,
+      [name]: value,
+    });
+  };
+
+  //funcion que limpia el formulario
   const clearForm = () => {
     setData(initialState);
   };
 
-      const handleSubmit = (e) => {
-        e.preventDefault();
-        //acciones formularios
-        console.log("Datos formulario", data);
-        localStorage.setItem("user", JSON.stringify(data))
-        //limpiamos formulario
-        clearForm(); //necesitamos el value en el input para poder modificar el status
-        setMessage("Formulario enviado con éxito");
-        setTimeout(() => {
-          navigate("/");
-        }, 3000);
-      };
+  const handleSubmit = (e) => {
+    e.preventDefault();
 
-    return (
-        <div>
-             <form onSubmit={handleSubmit}>
-      <input
-        type="text"
-        placeholder="name"
-        onChange={handleInputChange}
-        name="name"
-        value={data.name} //value conecta nuestro input con el estado. Nos permitira hacer el clear
-      />
-      <input
-        type="email"
-        placeholder="email"
-        onChange={handleInputChange}
-        name="email"
-        value={data.email}
-      />
-   <button disabled={btnDisabled} type="submit">
-          Enviar
-        </button>      {message}
-
-</form>
-        </div>
-    );
+    if (data.name.length < 3) {
+      setMessage("Name must be at least 3 characters");
+      setBtnDisabled(true); // Deshabilitar el botón si hay un error de validación
+    } else {
+      //acciones formularios
+      console.log("Datos formulario", data);
+      localStorage.setItem("user", JSON.stringify(data));
+      //limpiamos formulario
+     // clearForm(); //necesitamos el value en el input para poder modificar el status
+      setMessage("Formulario enviado con éxito");
+      setTimeout(() => {
+        setMessage(""); // Limpiamos el mensaje después de 3 segundos
+        navigate("/");
+      }, 3000);
+    }
   };
-  
- 
 
-export default Contact
+  return (
+    <div>
+      <form onSubmit={handleSubmit}>
+        <input
+          type="text"
+          placeholder="name"
+          onChange={handleInputChange}
+          name="name"
+          value={data.name}
+        />
+        <input
+          type="email"
+          placeholder="email"
+          onChange={handleInputChange}
+          name="email"
+          value={data.email}
+        />
+        <button disabled={btnDisabled} type="submit">
+          Enviar
+        </button>{" "}
+        {message}
+      </form>
+    </div>
+  );
+};
 
+export default Contact;
